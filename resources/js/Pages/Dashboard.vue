944324ace4e7cb302afe6f87bba25a66c6a09626
thutanyan <thutathuta319@gmail.com>
2024-12-01 09:32:15 +0630

    Game Initialized

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,32 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head,Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2
+                class="text-xl font-semibold leading-tight text-gray-800"
+            >
+                Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div
+                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
+                >
+                    <div class="p-6 text-gray-900">
+                        <Link :href="route('games.store')" method="post" class="inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900">
+                            Create Game
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
---
+++ resources/js/Pages/Games/Show.vue
@@ -0,0 +1,15 @@
+<template>
+    <AuthenticatedLayout>
+        Testing!!
+    </AuthenticatedLayout>
+</template>
+
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
